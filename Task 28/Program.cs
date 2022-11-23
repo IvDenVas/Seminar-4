227de617acfd5cbde02672437097946dba1963e3
Denis <ivdenvas@gmail.com>
2022-11-23 17:26:25 +0300

    finish task 28

---
+++ Task 28/Program.cs
@@ -0,0 +1,27 @@
+﻿// напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N
+Console.Clear();    
+int A = GetNumberFromUser("Введите целое число N: ", "Ошибка ввода!");
+int sumNumbers = GetSumNumbers(A);
+Console.Write($"{A} -> {sumNumbers}");
+
+int GetSumNumbers(int number)
+{
+    int res = 1;
+
+    for (int i = 1; i <= number; i++)
+        res = res * i;
+
+
+    return res;
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
